8e0f541c07e47dc130ba7b1b728e59d79fe1f3e6
Kiki <kiki@Mac.attlocal.net>
2025-04-29 16:08:06 -0400

    error

---
+++ src/app/error.tsx
@@ -0,0 +1,28 @@
+'use client'
+ 
+import { useEffect } from 'react'
+import { Button } from '@/components/ui/button'
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    console.error(error)
+  }, [error])
+ 
+  return (
+    <div>
+      <h2>There might be some trouble with Supabase... Please try refreshing the page!</h2>
+      <Button
+        onClick={
+          () => reset();
+        }
+      >
+        Try again
+      </Button>
+    </div>
+  )
+}
