29cb2adffde90e006a7aca85dfeced416c120648
Kiki <kiki@Mac.attlocal.net>
2025-05-01 10:34:26 -0400

    prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient | undefined;
+    var prisma: PrismaClient | undefined;
 }
 
 const prismaClientSingleton = () => new PrismaClient({
-  log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    datasources: {
+        db: {
+        url: process.env.POSTGRES_URL_NON_POOLING,
+        },
+    },
 });
 
-export const prisma =
-  globalThis.prisma || prismaClientSingleton();
+export const prisma = globalThis.prisma || prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
